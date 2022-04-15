6cd623b3b5f3a94d1c19bff2e94dfc538f3251a8
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2022-04-15 18:38:34 +0200

    Added Header/Animations/Style

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// getting the nav element
+const navigation = document.querySelector('.navigation');
+
+// getting exact position of X and Y of the element
+const rectNavigation = navigation.getBoundingClientRect();
+
+console.log(window.scrollY, rectNavigation.y);
+
+// if (rectNavigation.top > 80) {
+//     navigation.classList.add('navigation-static');
+//     console.log('działa');
+// }
+const header = document.querySelector('.section-header');
+const topHelper = document.querySelector('.top-helper');
+const headerRect = header.getBoundingClientRect();
+
+const newFunction = () => {
+    if (window.scrollY === headerRect.bottom) {
+        navigation.classList.toggle('navigation-static');
+        navigation.classList.remove('navigation-slide-out');
+        console.log('tak');
+    } else {
+        navigation.classList.add('navigation-slide-out');
+        navigation.classList.remove('navigation-static');
+    }
+    
+};
+
+window.addEventListener('scroll', newFunction);
+
+
+topHelper.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
