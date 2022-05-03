6c66d4c68d8fd5d6d7d5a3855707af3792abfffd
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2022-05-03 22:23:21 +0200

    Add: media query for responsive design

--- app.js
+++ app.js
@@ -15,36 +15,48 @@ const contact = document.querySelector('.section-contact');
 
 // Getting the position of each section
 const headerPosition = header.getBoundingClientRect();
-const aboutUsPosition = aboutus.getBoundingClientRect();
-const offersPosition = offers.getBoundingClientRect();
-const pricePosition = price.getBoundingClientRect();
-const contactPosition = contact.getBoundingClientRect();
+const aboutUsPosition = aboutUs.getBoundingClientRect();
+// const offersPosition = offers.getBoundingClientRect();
+// const pricePosition = price.getBoundingClientRect();
+// const contactPosition = contact.getBoundingClientRect();
 
 // Functions for manipulation of the  navigation background color
 // Aboutus section
 const aboutUsAnimation = () => {
 	// if the position of window while scrolling equals to the position of the bottom part of header
 	// and the position of window while scrolling is bigger or equal to the headerbottom - 5px
-	if (window.scrollY === headerPosition.bottom || window.scrollY >= headerPosition.bottom) {
-		// add new class that makes the navigation stay static
+	// if (window.scrollY === headerPosition.bottom || window.scrollY === headerPosition.top) {
+		// } else if (window.scrollY === headerPosition.bottom - 5 || window.scrollY <= headerPosition.bottom) {
+			// 	navigation.classList.remove('navigation-static-blue');
+			// 	navigation.classList.add('navigation-slide-out');
+			// 	elementAnimated = false;
+			// 	console.log('No');
+			// }
+	if (window.scrollY >= headerPosition.bottom && window.scrollY <= aboutUsPosition.bottom) {
+		navigation.style.position = 'fixed';
+		navigation.style.width = '100%';
 		navigation.classList.remove('navigation-slide-out');
-		// remove previous animation
 		navigation.classList.add('navigation-static-blue');
+	} else {
+		console.log('yes');
 	}
 };
 
-// Offers section
-const offersAnimation = () => {
-	if (window.scrollY === aboutUsPosition.bottom || window.scrollY >= aboutUsPosition.bottom) {
-		navigation.classList.remove('navigation-static-blue')
-		navigation.classList.add('navigation-static-white');
-	}
-}
+// // Offers section
+// const offersAnimation = () => {
+// 	if (window.scrollY === aboutUsPosition.bottom || window.scrollY >= aboutUsPosition.bottom) {
+// 		navigation.classList.remove('navigation-static-blue')
+// 		navigation.classList.add('navigation-static-white');
+// 	} else {
+// 		navigation.classList.remove('navigation-static-white');
+// 		navigation.classList.add('navigation');
+// 	}
+// }
 
 // function for creating an animation for the navbar
 const animation = () => {
 	aboutUsAnimation();
-	offersAnimation();
+	// offersAnimation();
 };
 
 // calling the function for adding and removing extra classes while scrolling down (Y-axis)
